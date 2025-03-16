c9c936cdc3c7139d3415c578ec2e28bce539e138
Diogo Garcia <124640510+devgarciadiogo@users.noreply.github.com>
2025-03-16 18:25:00 -0300

    Escolhendo palavra e categoria de forma aleatória
    Criando os states para selecionar categorias e palavras

--- secret_word/src/App.jsx
+++ secret_word/src/App.jsx
@@ -23,8 +23,45 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name); //Define o estado inicial do jogo como 0
   const [words] = useState(wordsList); //Estado para armazenar a lista de palavras do jogo
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    //pick a random word
+    const word =
+      words[category][[Math.floor(Math.random() * words[category].length)]];
+
+    console.log(word);
+
+    return { word, category };
+  };
+
   // starts the secret word game
   const startGame = () => {
+    //pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
